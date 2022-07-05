9c1e47974d0da5fb62a82c55528b0afb26854600
v380dev <v380dev@gmail.com>
2022-07-05 22:50:17 +0300

    dev add toast to copied table

--- src/main/resources/static/js/copy.js
+++ src/main/resources/static/js/copy.js
@@ -1,8 +1,31 @@
-function copytable(el) {
-    var urlField = document.getElementById(el)
-    var range = document.createRange()
-    range.selectNode(urlField)
-    window.getSelection().addRange(range)
-    document.execCommand('copy')
-    window.getSelection().removeAllRanges()
+function copytable(el, idMessage, status) {
+    var urlField = document.getElementById(el);
+    var range = document.createRange();
+    range.selectNode(urlField);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+
+    setMessage(idMessage, status);
+}
+
+
+
+
+function setMessage(idmsg, status){
+//    var span = document.getElementById(idmsg);
+//    span.innerHTML = "таблиця скопійована в буфер";
+    document.getElementById(idmsg).disabled=status;
+//console.log(idmsg);
+//console.log(1);
+//window.alert(idmsg);
+//window.alert("таблиця скопійована");
+
+//    span.textContent="таблиця скопійована в буфер";
+//    span.innerText = "таблиця скопійована в буфер"
+//    span.outerText = "таблиця скопійована в буфер"
+//    span.style.color = "#FF0000"
+//txt=document.createTextNode("таблиця скопійована в буфер")
+//    span.appendChild(txt)
+
 }
