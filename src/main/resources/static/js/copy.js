98f2862711382a5e43951ce1a60590a758a0a2b4
v380dev <v380dev@gmail.com>
2022-07-07 23:29:33 +0300

    dev add label animation to copied

--- src/main/resources/static/js/copy.js
+++ src/main/resources/static/js/copy.js
@@ -1,31 +1,16 @@
-function copytable(el, idMessage, status) {
-    var urlField = document.getElementById(el);
+function copyTable(idTable, idMessage) {
+    var urlField = document.getElementById(idTable);
     var range = document.createRange();
     range.selectNode(urlField);
     window.getSelection().addRange(range);
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
 
-    setMessage(idMessage, status);
+    showMessage(idMessage);
 }
 
-
-
-
-function setMessage(idmsg, status){
-//    var span = document.getElementById(idmsg);
-//    span.innerHTML = "таблиця скопійована в буфер";
-    document.getElementById(idmsg).disabled=status;
-//console.log(idmsg);
-//console.log(1);
-//window.alert(idmsg);
-//window.alert("таблиця скопійована");
-
-//    span.textContent="таблиця скопійована в буфер";
-//    span.innerText = "таблиця скопійована в буфер"
-//    span.outerText = "таблиця скопійована в буфер"
-//    span.style.color = "#FF0000"
-//txt=document.createTextNode("таблиця скопійована в буфер")
-//    span.appendChild(txt)
-
-}
+function showMessage(idMessage){
+ var x = document.getElementById(idMessage);
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
